74ad3fe0ee52067bfda20761005207dc3ff4c9ae
Youssef <yousefkamel282@gmail.com>
2023-07-13 09:38:04 -0700

    joo

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main entry point
+ *
+ * Description printing
+ *
+ * return 1 : (not success)
+ *
+ **/
+
+int main(void)
+{	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, ch, 59);
+	return (1);
+}
