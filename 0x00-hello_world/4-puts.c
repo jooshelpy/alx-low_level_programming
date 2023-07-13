028257ed5411f7db54dfee15fff0a9151cef294d
Youssef <yousefkamel282@gmail.com>
2023-07-13 08:25:25 -0700

    joo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,13 @@
 /**
  * main - Entry point
  *
+ * A c program that prints with puts function
+ *
  *  Return: Always 0 (Success)
- */
+*/
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }	
